d7639871dc7714ca01a84af495fbda12eb297ba2
BradleyGam <bradleygamble20@gmail.com>
2023-06-18 13:18:29 -0400

    add: solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const sequence = parseInt(number);
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (sequence < 3) {
+        return 1;
+    }
 
+    let previous = 1;
+    let current = 1;
+
+    for (let i = 3; i <= sequence; i++) {
+        const next = previous + current;
+        previous = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
