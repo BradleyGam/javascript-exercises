f6c8b3cce0c4a2cf807a59eeadbb15759a9fbbff
BradleyGam <bradleygamble20@gmail.com>
2023-06-18 13:00:08 -0400

    add: solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let alphabeticString = string.split("").filter(char => /[a-zA-Z0-9]/.test(char)).join("").toLowerCase();
+    let reverseString = alphabeticString.split("").reverse().join("").toLowerCase();
+    return alphabeticString == reverseString;;
 };
 
 // Do not edit below this line
